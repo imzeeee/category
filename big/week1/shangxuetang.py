5d263fce1960680d3e8a2c4ca0687ba7b2595c8c
Qinsheng Zhan <156695530+imzeeee@users.noreply.github.com>
2024-03-16 10:37:21 +0800

    Add files via upload

---
+++ big/week1/shangxuetang.py
@@ -0,0 +1,17 @@
+print("hello world!")
+d = 10>20 or 10>5
+print(d)
+
+a = [40,20,30]
+print(10 not in a)
+
+c = '''
+i~
+love~
+shenzhen~
+'''
+e = 'ab\'AA\'cd'
+print(c)
+print(e)
+f = "py\nth\ton"
+print(f)
